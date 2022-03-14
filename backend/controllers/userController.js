c3100600bee0be7b12bdd349f314b07174134fad
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-14 11:05:01 +0630

    finished user enpoint register,login and get current login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,44 +5,41 @@ const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
 
 // generate token to response
-const generateToken = (id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'30d'})
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' })
 }
 // @desc Register a new User
 // @Routes /api/users/
 //  @access /Public
 //  @action POST method
-const registerUser=asyncHandler(async(req,res,next)=>{
+const registerUser = asyncHandler(async (req, res, next) => {
     // destructure the request body
-    const {username,email,password} = req.body;
+    const { username, email, password } = req.body;
     // validation
-    if(!username || ! email || ! password){
-        return next(new ErrorResponse("Please fill all the fields",400));
+    if (!username || !email || !password) {
+        return next(new ErrorResponse("Please fill all the fields", 400));
     }
     // check user exist or not with given email
-    const existingUser = await User.findOne({email});
-    if(existingUser)return next(new ErrorResponse(`User already exist with given email : ${email}`,400));
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return next(new ErrorResponse(`User already exist with given email : ${email}`, 400));
 
     // Hash password
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password,salt);
+    const hashPassword = await bcrypt.hash(password, salt);
     //  create new user
     const user = await User.create({
         username,
         email,
-        password:hashPassword,
+        password: hashPassword,
     });
 
-    if(user){
-        res.status(201).json({
-            _id:user._id,
-            username:user.username,
-            email:user.email,
-            token:generateToken(user._id)
-        })
-    }else{
-       return next(new ErrorResponse('Invalid User data',400))
-    }
+    if (!user) return next(new ErrorResponse(`Invalid user data`, 400));
+    res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user._id)
+    });
 });
 
 
@@ -51,16 +48,33 @@ const registerUser=asyncHandler(async(req,res,next)=>{
 // @Routes /api/users/
 //  @access /Public
 //  @action POST method
-const loginUser = asyncHandler(async(req,res,next)=>{})
+const loginUser = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+    //  look user with email
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) return next(new ErrorResponse(`Invalid Credential`, 401))
+
+    res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+    });
+})
 
 // @desc get current logined User profile
 // @Routes /api/users/
 //  @access Private
 //  @action GET method
-const getProfileMe = asyncHandler(async(req,res,next)=>{
-
+const getProfileMe = asyncHandler(async (req, res, next) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name,
+      }
+      res.status(200).json(user)
 })
-module.exports ={
+module.exports = {
     registerUser,
     loginUser,
     getProfileMe
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const { registerUser,loginUser,getProfileMe } = require('../controllers/userCont
 const router = express.Router();
 
 router.post("/",registerUser);
+router.post("/login",loginUser);
 
 
 module.exports = router;
