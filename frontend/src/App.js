33d9a9c52bb0be2226112d091f7782623207ee19
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-16 12:05:52 +0630

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NewTicket from './pages/NewTicket';
+import Register from './pages/Register';
+import Ticket from './pages/Ticket';
+import Tickets from './pages/Tickets';
 function App() {
   return (
     <>
       <Router>
         <div className='container'>
-    
+          <Header/>
+          <Routes>
+            <Route path='/'  element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+            {/* must be private routes */}
+            <Route path='/new-ticket' element={<NewTicket/>}/>
+            <Route  path='tickets' element={<Tickets/>}/>
+            <Route path='/ticket/:ticketId' element={<Ticket/>}/>
+          </Routes>
         </div>
       </Router>
     </>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import {FaSignInAlt, FaSignOutAlt, FaUser  } from "react-icons/fa";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const logout=()=>{
+        console.log("Logout....");
+    }
   return (
-    <div>Header</div>
+    <header className='header'>
+        <div className="logo">
+            <Link to="/">Desk Helper</Link>
+        </div>
+        <ul>
+            <li>
+                <button className='btn' onClick={logout}>
+                  <FaSignOutAlt/>Logout
+                </button>
+            </li>
+            <li>
+                <Link to="/login">
+                    <FaSignInAlt/>SignIn
+                </Link>
+            </li>
+            <li>
+                <Link to="/register">
+                    <FaUser/>Register
+                </Link>
+            </li>
+        </ul>
+    </header>
   )
 }
 
