6f77109076b886bf024fc8bcc693bcf00166289f
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-16 10:27:00 +0630

    add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+    
+        </div>
+      </Router>
+    </>
   );
 }
 
