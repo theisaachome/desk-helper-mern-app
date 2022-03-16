2c574e01a4ec263780a81198428d10403b9f3055
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-17 03:21:36 +0630

    add login and register pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Login = () => {
+    
+    // formData
+    const [formData, setFormData] = useState({
+        email:"",
+        password:""
+    });
+    // destructure formData
+    const {email,password} = formData;
+
+    // setup input data
+    const onChange = (e)=>{
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.name]:e.target.value
+        }))
+    }
+
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        const userData = {
+            email,
+            password
+        }
+    }
   return (
-    <div>Login</div>
+    <>
+        <section className="heading">
+            <h2>Login</h2>
+            <p>Please log in to get support</p>
+        </section>
+        <section className="form">
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input 
+                    type="email"
+                    name="email" 
+                    id="email"
+                    value={email} 
+                    onChange={onChange}
+                    required
+                    placeholder='Enter your email'
+                    className="form-control" />
+                </div>
+                <div className="form-group">
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    value={password} 
+                    onChange={onChange}
+                    className="form-control"
+                    required
+                    placeholder='Enter your password' />
+                </div>
+                <div className="form-group">
+                    <button className='btn btn-block'>Submit</button>
+                </div>
+            </form>
+        </section>
+    </>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,99 @@
-import React from 'react'
+import {useState} from 'react'
+import { toast } from 'react-toastify';
 
 const Register = () => {
+    // fields for register
+    const [formData, setFormData] = useState({
+        username:"",
+        email:"",
+        password:"",
+        password2:""
+
+    });
+    // destructure formData
+    const {username,email,password,password2} = formData;
+
+    // setup input data
+    const onChange=(e)=>{
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.name]:e.target.value
+        }));
+    }
+    // send data to server
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        //  check password match
+        if(password !== password2){
+            toast.error("Passwords do not match");
+        }else{
+            const userData = {
+                username,
+                email,
+                password,
+            }
+            // send data
+            console.log(userData);
+        }
+    }
+
   return (
-    <div>Register</div>
+    <>
+        <section className="heading">
+            <h2>Register</h2>
+            <p>Please create an account</p>
+        </section>
+        <section className="form">
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <input type="text" 
+                name="username" 
+                id="username" 
+                className='form-control'
+                onChange={onChange}
+                value={username}
+                required
+                placeholder='Enter your name' />
+            </div>
+            <div className="form-group">
+                <input 
+                type="email" 
+                className='form-control'
+                name="email" 
+                id="email"
+                value={email}
+                onChange={onChange}
+                placeholder="Enter your email"
+                required />
+            </div>
+            <div className="form-group">
+                <input 
+                type="password" 
+                className='form-control'
+                name="password" 
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Enter your password"
+                required />
+            </div>
+            <div className="form-group">
+                <input 
+                type="password" 
+                className='form-control'
+                name="password2" 
+                id="password2"
+                value={password2}
+                onChange={onChange}
+                placeholder="Confirme your password"
+                required />
+            </div>
+            <div className="form-group">
+                <button className='btn btn-block' type="submit">Submit</button>
+            </div>
+            </form>
+        </section>
+    </>
   )
 }
 
