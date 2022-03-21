2f225cf52e6f40a69caf67ef13ffb242304fc9b5
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-21 17:01:43 +0630

    ticket detail

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,8 +1,11 @@
-import {useState,useEffect} from 'react'
+import {useState,useEffect} from 'react';
+import { toast } from 'react-toastify';
 import Modal from "react-modal";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import { getTicket } from '../features/tickets/ticketSlice';
+import BackButton from '../components/BackButton';
 
 const customStyles = {
   content: {
@@ -21,6 +24,7 @@ Modal.setAppElement('#root')
 const Ticket = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [noteText, setNoteText] = useState('');
+
   const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.tickets
   )
@@ -36,6 +40,15 @@ const Ticket = () => {
   const closeModal = () => setModalIsOpen(false)
 
 
+  useEffect(() => {
+    if(isError){
+      toast.error(message);
+    }
+    dispatch(getTicket(ticketId));
+    // eslint-disable-next-line
+  }, [isError,message,ticketId]);
+
+  
   if (isLoading ) {
     return <Spinner />
   }
@@ -46,7 +59,27 @@ const Ticket = () => {
 
 
   return (
-    <div>Ticket</div>
+    <div className='ticket-page'>
+      <header className="ticket-header">
+      <BackButton url='/tickets' />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
+        </h3>
+        <h3>Product: {ticket.product}</h3>
+        <hr />
+        <div className='ticket-desc'>
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+        <h2>Notes</h2>
+      </header>
+    </div>
   )
 }
 
